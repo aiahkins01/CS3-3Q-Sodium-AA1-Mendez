aebf763aefa4538e243618766507a2b2eb33256d
aiahkins01 <efamendez22@src.pshs.edu.ph>
2025-01-22 19:46:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let Radius = 6,378,000;
+let Circumference = (2*PI*Radius);
+let surfaceArea = 4*PI*(Radius**2);
+let volume = (4/3)*PI*(Radius**3);
+
+document.write('The radius of the sphere is ' + Radius + '.' + '<br>');
+document.write('The circumference is ' + Circumference + '.' + '<br>');
+document.write('The surface area is ' + surfaceArea + '.' + '<br>");
+document.write('The volume is ' + volume + '.');
